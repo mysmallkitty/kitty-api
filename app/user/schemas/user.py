a556f1f25ae8d4141697716fb193a173c4e85f98
4vpr <4shout4@gmail.com>
2026-01-07 14:49:13 +0900

    feat: me에 순위 정보 추가

--- app/user/schemas/user.py
+++ app/user/schemas/user.py
@@ -7,9 +7,11 @@ UserOut = pydantic_model_creator(User, name="UserOut")
 
 class UserMe(UserOut):
     friend_count: int
+    rank: int
 
     @classmethod
     async def from_user(cls, user: User) -> "UserMe":
         base = await UserOut.from_tortoise_orm(user)
-        cnt = await Friendship.filter(user=user).count()
-        return cls.model_validate({**base.model_dump(), "friend_count": cnt})
+        friend_count = await Friendship.filter(user=user).count()
+        rank = await User.filter(skill_level__gt=user.skill_level).count() + 1
+        return cls.model_validate({**base.model_dump(), "friend_count": friend_count, "rank": rank})
